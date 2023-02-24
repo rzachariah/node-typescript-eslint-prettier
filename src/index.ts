c6231852e14038fba1fc01d2ac6866a648f0b71b
Ranjith Zachariah <ranjzach@amazon.com>
2023-02-24 15:14:24 -0500

    Enhance demo app

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
+const bag = {
+  fruit: "apple",
+  vegetable: "carrot",
+};
+console.log("bag", bag);
+
+const fruitFoods = [
+  "apple juice",
+  "orange marmalade",
+  "pear tart",
+  "grape crush",
+  "pinot colada",
+];
+console.log("fruitFoods", fruitFoods);
+
 function sayMyName(name: string): void {
   if (name === "Heisenberg") {
     console.log("You're right 👍");
@@ -5,5 +20,5 @@ function sayMyName(name: string): void {
     console.log("You're wrong 👎");
   }
 }
- 
+
 sayMyName("Heisenberg");
